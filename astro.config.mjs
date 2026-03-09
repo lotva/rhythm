35e50d43fafe07d28cbfa9eec0f679d6f2abe5db
lotva <dlotva@gmail.com>
2026-03-09 15:42:06 +0300

    feat: add MDX support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 
+import mdx from '@astrojs/mdx'
 import unocss from 'unocss/astro'
 
 export default defineConfig({
-	integrations: [unocss()],
+	integrations: [mdx(), unocss()],
+
+	markdown: {
+		shikiConfig: {
+			themes: {
+				light: 'vitesse-light',
+				dark: 'vitesse-dark',
+			},
+		},
+	},
 })
