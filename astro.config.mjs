96a919328faad8e89d1a2ecfe62c05bbaac587d0
lotva <dlotva@gmail.com>
2026-03-10 17:02:34 +0300

    feat: process text with Typograf

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,23 @@
 import { defineConfig } from 'astro/config'
 
 import mdx from '@astrojs/mdx'
+import typograf from 'astro-typograf'
 import unocss from 'unocss/astro'
 
 export default defineConfig({
-	integrations: [mdx(), unocss()],
+	integrations: [
+		mdx(),
+		unocss(),
+
+		process.env.NODE_ENV === 'production'
+			? typograf({
+					typografOptions: {
+						locale: ['ru', 'en-US'],
+						disableRule: ['common/space/trimLeft', 'common/space/trimRight'],
+					},
+				})
+			: null,
+	],
 
 	markdown: {
 		shikiConfig: {
